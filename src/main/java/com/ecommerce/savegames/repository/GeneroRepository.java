05f6e41ae3896840917474f3d44a47420b6d4e71
marcelooo616 <marcelooo2014@hotmail.com>
2022-09-26 20:30:08 -0300

    revisando codigo

--- src/main/java/com/ecommerce/savegames/repository/GeneroRepository.java
+++ src/main/java/com/ecommerce/savegames/repository/GeneroRepository.java
@@ -3,6 +3,7 @@ package com.ecommerce.savegames.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ecommerce.savegames.model.Genero;
@@ -10,5 +11,5 @@ import com.ecommerce.savegames.model.Genero;
 @Repository
 public interface GeneroRepository extends JpaRepository<Genero, Long>{
 
-	public List<Genero> findAllByDescricaoContainingIgnoreCase(String descricao);
+	public List<Genero> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 }
